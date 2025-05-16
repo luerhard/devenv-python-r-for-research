4dc2687ed12d9b6583caf9decfc6bbaebc1cd849
Lukas Erhard <luerhard@googlemail.com>
2025-05-17 01:37:48 +0200

    test on linux

--- flake.nix
+++ flake.nix
@@ -1,7 +1,6 @@
 {
   inputs = {
     nixpkgs.url = "github:cachix/devenv-nixpkgs/rolling";
-    systems.url = "github:nix-systems/default";
     devenv.url = "github:cachix/devenv";
     devenv.inputs.nixpkgs.follows = "nixpkgs";
     nixpkgs-python.url = "github:cachix/nixpkgs-python";
@@ -22,114 +21,97 @@
       nixpkgs,
       devenv,
       nix-gl-host,
-      systems,
       ...
     }@inputs:
     let
-      forEachSystem = nixpkgs.lib.genAttrs (import systems);
+      system = "aarch64-darwin";
+      pkgs = nixpkgs.legacyPackages.${system};
+      rEnv = pkgs.rWrapper.override {
+        packages = with pkgs.rPackages; [
+          # always deps
+          box
+          reticulate
+          svglite
+          tidyverse
+          IRkernel
+          jsonlite
+          here
+          languageserver
+          lintr
+        ];
+      };
+      kernelsDir = ".devenv/.jupyter/kernels";
 
     in
     {
-      packages = forEachSystem (system: {
-        devenv-up = self.devShells.${system}.default.config.procfileScript;
-        devenv-test = self.devShells.${system}.default.config.test;
-      });
+      packages.${system}.devenv-up = self.devShells.${system}.default.config.procfileScript;
 
-      devShells = forEachSystem (
-        system:
-        let
-          pkgs = import nixpkgs {
-            inherit system;
-            config = {
-              allowUnfree = true; # necessary for CUDA
-            };
-            overlays = [
-              (import ./nix/python-overlay.nix)
-              (import ./nix/r-overlay.nix)
-            ];
-          };
-          rEnv = pkgs.rWrapper.override {
-            packages = with pkgs.rPackages; [
-              # always deps
-              box
-              reticulate
-              svglite
-              tidyverse
-              IRkernel
-              jsonlite
-              here
-              languageserver
-              lintr
-            ];
-          };
-          kernelsDir = ".devenv/.jupyter/kernels";
-        in
-        {
-          default = devenv.lib.mkShell {
-            inherit inputs pkgs;
-            modules = [
-              {
-                languages.python = {
-                  enable = true;
-                  version = "3.12.2";
-                  uv.enable = true;
-                };
+      devShells.${system}.default = devenv.lib.mkShell {
+        inherit inputs pkgs;
+        modules = [
+          (
+            { pkgs, config, ... }:
+            {
+              languages.python = {
+                enable = true;
+                version = "3.12.2";
+                uv.enable = true;
+              };
 
-                languages.r = {
-                  enable = true;
-                  package = rEnv;
-                };
+              languages.r = {
+                enable = true;
+                package = rEnv;
+              };
 
-                # https://devenv.sh/reference/options/
-                packages =
-                  [
-                    pkgs.gcc
-                    pkgs.git
-                  ]
-                  ++ pkgs.lib.lists.optional pkgs.stdenv.isLinux [
-                    nix-gl-host.defaultPackage.${system}
-                  ];
+              # https://devenv.sh/reference/options/
+              packages =
+                [
+                  pkgs.gcc
+                  pkgs.git
+                ]
+                ++ pkgs.lib.lists.optional pkgs.stdenv.isLinux [
+                  nix-gl-host.defaultPackage.${system}
+                ];
 
-                scripts.setup-jupyter.exec = ''
-                  echo "Setting up R kernel for Jupyter..."
+              scripts.setup-jupyter.exec = ''
+                echo "Setting up R kernel for Jupyter..."
 
-                  # Ensure an 'ir' folder exists in 'KernelsDir':
-                  mkdir -p "${kernelsDir}/ir"
+                # Ensure an 'ir' folder exists in 'KernelsDir':
+                mkdir -p "${kernelsDir}/ir"
 
-                  # Copy the files using interpolation
-                  cp -r ${pkgs.rPackages.IRkernel}/library/IRkernel/kernelspec/* "${kernelsDir}/ir"
+                # Copy the files using interpolation
+                cp -r ${pkgs.rPackages.IRkernel}/library/IRkernel/kernelspec/* "${kernelsDir}/ir"
 
-                  # Add write permission
-                  chmod -R u+w "${kernelsDir}/ir"
+                # Add write permission
+                chmod -R u+w "${kernelsDir}/ir"
 
-                  # set up Jupyter to look for kernels in the '.jupyter' dir:
-                  echo "Jupyter R kernel is ready."
+                # set up Jupyter to look for kernels in the '.jupyter' dir:
+                echo "Jupyter R kernel is ready."
 
-                  uv run python -m ipykernel install --prefix="/tmp" --name="python" --display-name="Python" 2> /dev/null
-                  cp -r /tmp/share/jupyter/kernels/python ${kernelsDir}/
+                uv run python -m ipykernel install --prefix="/tmp" --name="python" --display-name="Python" 2> /dev/null
+                cp -r /tmp/share/jupyter/kernels/python ${kernelsDir}/
 
-                  echo "Python (devenv) kernel is ready."
-                '';
+                echo "Python (devenv) kernel is ready."
+              '';
 
-                processes = {
-                  jupyter.exec = "uv run jupyter notebook --no-browser --ip='localhost' --IdentityProvider.token='' --ServerApp.password=''";
-                };
+              processes = {
+                jupyter.exec = "uv run jupyter notebook --no-browser --ip='localhost' --IdentityProvider.token='' --ServerApp.password=''";
+              };
 
-                enterShell = ''
-                  setup-jupyter
-                  export JUPYTER_PATH="$PWD/.devenv/.jupyter"
-                  export RETICULATE_PYTHON=$(uv run python -c "import sys; print(sys.executable)")
-                '';
+              enterShell = ''
+                setup-jupyter
+                export JUPYTER_PATH="$PWD/.devenv/.jupyter"
+                export RETICULATE_PYTHON=$(uv run python -c "import sys; print(sys.executable)")
+              '';
 
-                enterTest = ''
-                  echo "Running tests"
-                  git --version | grep --color=auto "${pkgs.git.version}"
-                  uv run pytest
-                '';
-              }
-            ];
-          };
-        }
-      );
+              enterTest = ''
+                echo "Running tests"
+                git --version | grep --color=auto "${pkgs.git.version}"
+                uv run pytest
+              '';
+            }
+          )
+        ];
+      };
     };
 }

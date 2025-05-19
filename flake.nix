56978620495babf321e2bffda0650fc0afb79377
Lukas Erhard <luerhard@googlemail.com>
2025-05-19 20:23:21 +0200

    just use uv for python, don't additionally use venv

--- flake.nix
+++ flake.nix
@@ -8,6 +8,7 @@
       nixpkgs.follows = "nixpkgs";
     };
     nix-gl-host.url = "github:numtide/nix-gl-host";
+    flake-utils.url = "github:numtide/flake-utils";
   };
 
   nixConfig = {
@@ -21,141 +22,113 @@
       nixpkgs,
       devenv,
       nix-gl-host,
+      flake-utils,
       ...
     }@inputs:
-    let
-      system = "x86_64-linux";
-      pkgs = nixpkgs.legacyPackages.${system};
-      rEnv = pkgs.rWrapper.override {
-        packages = with pkgs.rPackages; [
-          # always deps
-          box
-          reticulate
-          svglite
-          tidyverse
-          IRkernel
-          jsonlite
-          here
-          languageserver
-          lintr
-        ];
-      };
-      kernelsDir = ".devenv/.jupyter/kernels";
+    flake-utils.lib.eachSystem [ "aarch64-darwin" "x86_64-linux" ] (
+      system:
+      let
+        pkgs = import nixpkgs {
+          inherit system;
+        };
 
-    in
-    {
-      packages.${system}.devenv-up = self.devShells.${system}.default.config.procfileScript;
+        kernelsDir = ".devenv/.jupyter/kernels";
 
-      devShells.${system}.default = devenv.lib.mkShell {
-        inherit inputs pkgs;
-        modules = [
-          (
-            { pkgs, config, ... }:
-            {
-              # system dependencies
-              packages =
-                [
-                  # always system deps
-                  pkgs.gcc
-                  pkgs.glibcLocales
-                  # weird build deps for rpy2
-                  pkgs.libdeflate
-                  pkgs.zlib
-                  pkgs.xz
-                  pkgs.bzip2
-                  pkgs.libdeflate
-                  pkgs.icu
-                  # rpy2 deps end
-                  pkgs.git
-                  # project specific deps below
-                ]
-                ++ pkgs.lib.optionals pkgs.stdenv.isLinux [
-                  nix-gl-host.defaultPackage.${pkgs.system}
-                ];
+      in
+      {
+        # packages.devenv-up = self.devShells.default.config.procfileScript;
 
-              # R dependencies
-              languages.r = {
-                enable = true;
-                package = pkgs.rWrapper.override {
-                  packages = with pkgs.rPackages; [
-                    # always deps
-                    box
-                    reticulate
-                    svglite
-                    tidyverse
-                    IRkernel
-                    jsonlite
-                    here
-                    languageserver
-                    lintr
-                    # custom deps
-
+        devShells.default = devenv.lib.mkShell {
+          inherit inputs pkgs;
+          modules = [
+            (
+              { pkgs, config, ... }:
+              {
+                # system dependencies
+                packages =
+                  [
+                    # always system deps
+                    pkgs.git
+                    pkgs.gcc
+                    # weird build deps for rpy2
+                    pkgs.bzip2
+                    pkgs.icu
+                    pkgs.libdeflate
+                    pkgs.xz
+                    pkgs.zlib
+                    # # rpy2 deps end
+                  ]
+                  ++ pkgs.lib.lists.optional pkgs.stdenv.isLinux [
+                    nix-gl-host.defaultPackage.${system}
                   ];
+
+                # R dependencies
+                languages.r = {
+                  enable = true;
+                  package = pkgs.rWrapper.override {
+                    packages = with pkgs.rPackages; [
+                      # always deps
+                      box
+                      reticulate
+                      svglite
+                      tidyverse
+                      IRkernel
+                      jsonlite
+                      here
+                      languageserver
+                      lintr
+                      # custom deps
+
+                    ];
+                  };
                 };
-              };
 
-              # python dependencies
-              languages.python = {
-                enable = true;
-                version = "3.12";
-                venv.enable = true;
-                venv.requirements = ''
-                  # always deps
-                  pip
-                  jupyter
-                  rpy2
-                  # project-specific deps below
-                  numpy
-                  pandas
-                  torch
-                  multiprocessing-logging
-                  spacy
+                # python dependencies
+                languages.python = {
+                  enable = true;
+                  version = "3.12";
+                  uv.enable = true;
+                };
+
+                processes = {
+                  jupyter.exec = ''
+
+                    echo "Setting up R kernel for Jupyter..."
+
+                    kernelsDir=.devenv/.jupyter/kernels
+
+                    # Ensure an 'ir' folder exists in 'KernelsDir':
+                    echo "Ensuring folder exists"
+                    mkdir -p $kernelsDir/ir
+
+                    # Copy the files using interpolation
+                    cp -r ${pkgs.rPackages.IRkernel}/library/IRkernel/kernelspec/* $kernelsDir/ir
+
+                    # Add write permission
+                    chmod -R u+w $kernelsDir/ir
+
+                    # set up Jupyter to look for kernels in the '.jupyter' dir:
+                    echo "Jupyter R kernel is ready."
+
+                    uv run python -m ipykernel install --prefix="/tmp" --name="python" --display-name="Python" > /dev/null 2>&1
+                    cp -r /tmp/share/jupyter/kernels/python $kernelsDir/
+
+                    echo "Python (devenv) kernel is ready."
+
+                    uv run jupyter notebook --no-browser --ip="localhost" --IdentityProvider.token="" --ServerApp.password=""
+                  '';
+                };
+
+                enterShell = ''
+                  export JUPYTER_PATH="$PWD/.devenv/.jupyter"
+                  export RETICULATE_PYTHON=$(uv run python -c "import sys; print(sys.executable)")
                 '';
-                uv.enable = true;
-              };
 
-              processes = {
-                jupyter.exec = ''
-
-                  echo "Setting up R kernel for Jupyter..."
-
-                  kernelsDir=.devenv/.jupyter/kernels
-
-                  # Ensure an 'ir' folder exists in 'KernelsDir':
-                  echo "Ensuring folder exists"
-                  mkdir -p $kernelsDir/ir
-
-                  # Copy the files using interpolation
-                  cp -r ${pkgs.rPackages.IRkernel}/library/IRkernel/kernelspec/* $kernelsDir/ir
-
-                  # Add write permission
-                  chmod -R u+w $kernelsDir/ir
-
-                  # set up Jupyter to look for kernels in the '.jupyter' dir:
-                  echo "Jupyter R kernel is ready."
-
-                  uv run python -m ipykernel install --prefix="/tmp" --name="python" --display-name="Python" > /dev/null 2>&1
-                  cp -r /tmp/share/jupyter/kernels/python $kernelsDir/
-
-                  echo "Python (devenv) kernel is ready."
-
-                  uv run jupyter notebook --no-browser --ip="localhost" --IdentityProvider.token="" --ServerApp.password=""
-                '';
-              };
-
-              enterShell = ''
-                export JUPYTER_PATH="$PWD/.devenv/.jupyter"
-                export RETICULATE_PYTHON=$(uv run python -c "import sys; print(sys.executable)")
-              '';
-
-              enterTest = ''
-                echo "THIS IS MY ACTUAL CODE THAT WOULD RUN: (IS CUDA AVAILABLE?)"
-                nixglhost uv run python -c "import torch; print(torch.cuda.is_available())"
-                echo "OWN CODE DONE"
-              '';
-            }
-          )
-        ];
-      };
-    };
+                # enterTest = '''';
+              }
+            )
+          ];
+        };
+      }
+    );
 }
---
+++ main.py
@@ -0,0 +1,6 @@
+def main():
+    print("Hello from devenv-python-r-for-research!")
+
+
+if __name__ == "__main__":
+    main()

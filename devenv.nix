a23a721a799318ae0114016a38d92609cb3a22e1
Lukas Erhard <luerhard@googlemail.com>
2025-05-19 18:12:24 +0200

    add test stuff

--- devenv.nix
+++ devenv.nix
@@ -4,10 +4,16 @@
   packages = [
     # always system deps
     pkgs.gcc
+    # weird build deps for rpy2
+    pkgs.libdeflate
+    pkgs.zlib
+    pkgs.xz
+    pkgs.bzip2
     pkgs.libdeflate
     pkgs.icu
+    # rpy2 deps end
     pkgs.git
-    # custom system deps
+    # project specific deps below
   ] ++ pkgs.lib.optionals pkgs.stdenv.isLinux [
     nix-gl-host.defaultPackage.${pkgs.system}
   ];
@@ -39,14 +45,16 @@
     version = "3.12";
     venv.enable = true;
     venv.requirements = ''
+      # always deps
       pip
       jupyter
+      rpy2
+      # project-specific deps below
       numpy
       pandas
       torch
       multiprocessing-logging
       spacy
-      rpy2
     '';
     uv.enable = true;
   };
@@ -64,18 +72,16 @@
     mkdir -p $kernelsDir/ir
 
     # Copy the files using interpolation
-    echo "copying files"
     cp -r ${pkgs.rPackages.IRkernel}/library/IRkernel/kernelspec/* $kernelsDir/ir
 
     # Add write permission
-    echo "change perms"
     chmod -R u+w $kernelsDir/ir
 
     # set up Jupyter to look for kernels in the '.jupyter' dir:
     echo "Jupyter R kernel is ready."
 
     uv run python -m ipykernel install --prefix="/tmp" --name="python" --display-name="Python" > /dev/null 2>&1
-    cp -r /tmp/share/jupyter/kernels/python kernelsDir/
+    cp -r /tmp/share/jupyter/kernels/python $kernelsDir/
 
     echo "Python (devenv) kernel is ready."
 
@@ -87,6 +93,11 @@
     export JUPYTER_PATH="$PWD/.devenv/.jupyter"
     export RETICULATE_PYTHON=$(uv run python -c "import sys; print(sys.executable)")
   '';
-
+  
+  enterTest = ''
+    echo "THIS IS MY ACTUAL CODE THAT WOULD RUN: (IS CUDA AVAILABLE?)"
+    nixglhost uv run python -c "import torch; print(torch.cuda.is_available())"
+    echo "OWN CODE DONE"
+  '';
 
 }
